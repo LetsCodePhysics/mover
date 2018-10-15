938af817d2300d60595ef3e3b6ad7a4d545fb831
LetsCodePhysics <letscodephysics@gmail.com>
2018-10-15 08:38:03 -0400

    Update mover.py

--- mover.py
+++ mover.py
@@ -1,13 +1,9 @@
-def move(duration,delta_t,object, *objects):
+def move(duration,delta_t,args):
     time = 0
     while (time < duration):
-        rate(50)
-        object.velocity = object.velocity + object.force/object.mass*delta_t
-        object.pos = object.pos + object.velocity*delta_t
-        for obj in objects:
-            obj.velocity = obj.velocity + obj.force/obj.mass*delta_t
-            obj.pos = obj.pos + obj.velocity*delta_t
-        
-        
-        time = time + delta_t
+      rate(50)
+      for obj in args:
+        obj.velocity = obj.velocity + obj.force/obj.mass*delta_t
+        obj.pos = obj.pos + obj.velocity*delta_t
+      time = time + delta_t
     return
